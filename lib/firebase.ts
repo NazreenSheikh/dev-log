516278030dfdab4b9c6be3135699d6f107503e08
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-10 23:32:57 +0530

    firebase file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,7 +5,7 @@ import "firebase/storage"
 
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -13,7 +13,8 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 
-  };
+};
+
 
   if (!firebase.apps.length){
       firebase.initializeApp(firebaseConfig);
@@ -22,3 +23,37 @@ const firebaseConfig = {
   export const auth=firebase.auth();
   export const firestore=firebase.firestore();
   export const storage=firebase.storage();
+  export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+ export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+ export const increment = firebase.firestore.FieldValue.increment;
+
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export const STATE_CHANGED = firebase.storage.TaskEvent.STATE_CHANGED;
+
+/// Helper functions
+
+/**`
+ * Gets a users/{uid} document with username
+ * @param  {string} username
+ */
+ export async function getUserWithUsername(username) {
+    const usersRef = firestore.collection('users');
+    const query = usersRef.where('username', '==', username).limit(1);
+    const userDoc = (await query.get()).docs[0];
+    return userDoc;
+  }
+
+  /**`
+   * Converts a firestore document to JSON
+   * @param  {DocumentSnapshot} doc
+   */
+  export function postToJSON(doc) {
+    const data = doc.data();
+    return {
+      ...data,
+      // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+      createdAt: data?.createdAt.toMillis() || 0,
+      updatedAt: data?.updatedAt.toMillis() || 0,
+    };
+  }
