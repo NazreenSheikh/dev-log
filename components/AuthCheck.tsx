7b82529efcd57e07de9a7838a14b8c786624d319
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-10 23:31:18 +0530

    Auth checker comp

--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -1,10 +1,22 @@
-import Link from 'next/link';
-import { useContext } from 'react';
-import { UserContext } from '@lib/context';
+import Link from 'next/link'
+import { useContext } from 'react'
+import { UserContext } from '@lib/context'
+
+interface AuthCheckProps {
+  children
+  fallback?: JSX.Element
+}
 
 // Component's children only shown to logged-in users
-export default function AuthCheck(props) {
-  const { username } = useContext(UserContext);
+export default function AuthCheck({
+  children,
+  fallback,
+}: AuthCheckProps): JSX.Element {
+  const { username } = useContext(UserContext)
 
-  return username ? props.children : props.fallback || <Link href="/enter">You must be signed in</Link>;
+  if (username) {
+    return children
+  }
+
+  return fallback || <Link href="/login">You must be signed in</Link>
 }
