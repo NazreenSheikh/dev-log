739c54cf37d785d75d619b10602d47f52bb5262e
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-11 07:40:27 +0530

    add: all  pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar';
+import {Toaster} from 'react-hot-toast';
+import {UserContext} from "../lib/context";
+import { useUserData } from '../lib/useAuthState';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const{user, username}=useUserData()
+  console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+    console.log("hiiiiiiiiiiiii")
+  return (<>
+  <UserContext.Provider value={{user, username}}>
+    <Navbar/>
+      <Toaster/>
+      <Component {...pageProps} />
+  </UserContext.Provider>
+    </>)
+
 }
 
 export default MyApp
